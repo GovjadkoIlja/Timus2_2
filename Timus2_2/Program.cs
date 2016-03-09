2b36f0bbbea8f022e65f22f6706c78477fd4c578
GovjadkoIlja <govjadkoilja@yandex.ru>
2016-03-09 19:29:48 +0300

    .

---
+++ Timus2_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timus2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m=-2000000000;
+            n=Convert.ToInt32( Console.ReadLine());
+            int[] mass = new int[n+1];
+            mass[0] = 0;
+            mass[1] = 1;
+            for (int i=2; i< n+1; i++)
+            {
+                mass[i] = (i % 2 == 0 ? mass[i / 2] : mass[(i - 1) / 2] + mass[(i + 1) / 2]);
+                if (mass[i] > m)
+                    m = mass[i];
+            }
+            Console.Write(m);
+            Console.ReadKey();
+        }
+    }
+}
